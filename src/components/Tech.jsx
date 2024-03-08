46291721f29798c0cfdc2d6f45961929cc8ad049
Narendran <naren.cse008@gmail.com>
2024-03-08 15:54:07 +0530

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,23 +7,36 @@ import { fadeIn, textVariant } from "../utils/motion"
 
 
 const Tech = () => {
+  const isMobile = window.matchMedia("(max-width: 500px)").matches;
+  const Map = (isMobile ,technologies)=>{
+    if(isMobile){
+      return(
+    technologies.slice(0,4).map((technology)=>(
+    <div className='w-28 h-28' key={technology.name}>
+    <BallCanvas icon={technology.icon}/>
+    <p className="flex justify-center text-white font-bold">{technology.name}</p>
+    </div>
+    )))
+    }
+    else{return(
+      technologies.map((technology)=>(
+        <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon}/>
+        <p className="flex justify-center text-white font-bold">{technology.name}</p>
+        </div>
+      )))
+    }
+  }
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>what i have learnt so far</p>
-        <h1 className={styles.sectionHeadText}>Technology Stack</h1>        
-      </motion.div>
-      <motion.div className="flex flex-row flex-wrap justify-center gap-10 mt-10" >
-          {
-          technologies.map((technology, index) => (
-              <motion.div className="w-28 h-28" key={technology.name} variants={fadeIn("up", "spring", index*0.2, 1)}>
-              <BallCanvas icon={ technology.icon } />
-              </motion.div>
-            ))
-          }
-      </motion.div>
+    <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {Map(isMobile ,technologies)}
+    </div>
     </>
-  )
-}
+  );
+};
+
+
 
 export default SectionWrapper(Tech, "")
